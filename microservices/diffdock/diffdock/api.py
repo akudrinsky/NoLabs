fe1011272b0451ac2e05e01084d735eb68b10c9e
jt <jaktenstid1@gmail.com>
2024-04-06 18:52:21 +0300

    feat: malevich for diffdock

--- microservices/diffdock/diffdock/api.py
+++ microservices/diffdock/diffdock/api.py
@@ -6,9 +6,11 @@ app = FastAPI(
     title="Diff Dock"
 )
 
-from diffdock.loggers import Log
 from diffdock.job_state_manager import job_state_manager
+from malevich.square import processor
 
+
+@processor()
 @app.post("/run-docking")
 def predict(request: RunDiffDockPredictionRequest) -> RunDiffDockPredictionResponse:
     if request.job_id:
@@ -18,10 +20,14 @@ def predict(request: RunDiffDockPredictionRequest) -> RunDiffDockPredictionRespo
         job_state_manager.finish_job(request.job_id)
     return result
 
+
+@processor()
 @app.get("/job/{job_id}/is-running")
 def is_job_running(job_id: str) -> IsJobRunningResponse:
     return IsJobRunningResponse(is_running=job_state_manager.is_job_running(job_id))
 
+
+@processor()
 @app.get("/jobs/running")
 def get_running_jobs():
     return {"running_jobs": job_state_manager.get_running_jobs()}
